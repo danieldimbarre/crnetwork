5b087b4f211383c8f37e1aa5a9f291727f02f7af
summeRz <contatosummerz@gmail.com>
2022-07-08 14:50:29 -0300

    Arquivos da hud.

---
+++ resources/[scripts]/hud/server-side/core.lua
@@ -0,0 +1,57 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- VRP
+-----------------------------------------------------------------------------------------------------------------------------------------
+local Tunnel = module("vrp","lib/Tunnel")
+local Proxy = module("vrp","lib/Proxy")
+vRPC = Tunnel.getInterface("vRP")
+vRP = Proxy.getInterface("vRP")
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- CONNECTION
+-----------------------------------------------------------------------------------------------------------------------------------------
+cRP = {}
+Tunnel.bindInterface("hud",cRP)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- WEATHERLIST
+-----------------------------------------------------------------------------------------------------------------------------------------
+GlobalState["Work"] = 0
+GlobalState["Hours"] = 0
+GlobalState["Minutes"] = 0
+GlobalState["Weather"] = "EXTRASUNNY"
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- THREADSYNC
+-----------------------------------------------------------------------------------------------------------------------------------------
+CreateThread(function()
+	while true do
+		GlobalState["Work"] = GlobalState["Work"] + 1
+		GlobalState["Minutes"] = GlobalState["Minutes"] + 1
+
+		if GlobalState["Minutes"] >= 60 then
+			GlobalState["Hours"] = GlobalState["Hours"] + 1
+			GlobalState["Minutes"] = 0
+
+			if GlobalState["Hours"] >= 24 then
+				GlobalState["Hours"] = 0
+			end
+		end
+
+		Wait(10000)
+	end
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- TIMESET
+-----------------------------------------------------------------------------------------------------------------------------------------
+RegisterCommand("timeset",function(source,args)
+	if exports["chat"]:statusChat(source) then
+		local Passport = vRP.Passport(source)
+		if Passport then
+			if vRP.hasGroup(Passport,"Admin") then
+				GlobalState["Hours"] = parseInt(args[1])
+				GlobalState["Minutes"] = parseInt(args[2])
+
+				if args[3] then
+					GlobalState["Weather"] = args[3]
+				end
+			end
+		end
+	end
+end)

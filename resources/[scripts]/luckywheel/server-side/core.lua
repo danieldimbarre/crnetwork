68dc8a92272e021c3c6a511690877bfe11831980
Alan <alansoaressobral@gmail.com>
2022-09-28 17:31:05 -0300

    Initial commit

--- resources/[scripts]/luckywheel/server-side/core.lua
+++ resources/[scripts]/luckywheel/server-side/core.lua
@@ -35,7 +35,7 @@ function Creative.checkRolling()
 				return true
 			end
 
-			if vRP.PaymentFull(source,Passport,5000) then
+			if vRP.PaymentFull(Passport,source,5000) then
 				Active = os.time() + 20
 				return true
 			else

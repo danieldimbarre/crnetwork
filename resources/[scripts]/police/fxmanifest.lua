9197d75f8f9982e68bbc895d8210fb177116a29e
Alan <alansoaressobral@gmail.com>
2022-09-29 12:20:30 -0300

    Initial commit

--- resources/[scripts]/police/fxmanifest.lua
+++ resources/[scripts]/police/fxmanifest.lua
@@ -2,8 +2,11 @@ fx_version "bodacious"
 game "gta5"
 lua54 "yes"
 
+ui_page "web-side/index.html"
+
 client_scripts {
 	"@vrp/config/Native.lua",
+	"@PolyZone/client.lua",
 	"@vrp/lib/Utils.lua",
 	"client-side/*"
 }
@@ -12,4 +15,9 @@ server_scripts {
 	"@vrp/config/Item.lua",
 	"@vrp/lib/Utils.lua",
 	"server-side/*"
+}
+
+files {
+	"web-side/*",
+	"web-side/**/*"
 }

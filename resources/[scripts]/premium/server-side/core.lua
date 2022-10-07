c11bc25f15a9e645411fabc3f1edeb142ffbc1c4
Alan <alansoaressobral@gmail.com>
2022-10-07 09:53:30 -0300

    Initial commit

--- resources/[scripts]/premium/server-side/core.lua
+++ resources/[scripts]/premium/server-side/core.lua
@@ -364,15 +364,6 @@ for i = 1,#HackerEvents do
 		local Passport = vRP.Passport(source)
 		if Passport then
 			TriggerEvent("Discord","Hackers","**Source:** "..source.."\n**Passaporte:** "..Passport.."\n**Motivo:** Carregou um evento da blacklist.\n**Address:** "..GetPlayerEndpoint(source),3092790)
-
-			if not vRP.HasGroup(Passport,"Admin") then
-				local Identity = vRP.Identity(Passport)
-				if Identity then
-					vRP.Kick(Passport,"Banido.")
-					vRP.Query("banneds/InsertBanned",{ license = Identity["license"], time = 999999 })
-				end
-			end
-
 			CancelEvent()
 		end
 	end)
@@ -386,14 +377,6 @@ AddEventHandler("admin:Print",function(Message)
 	local Passport = vRP.Passport(source)
 	if Passport then
 		TriggerEvent("Discord","Hackers","**Source:** "..source.."\n**Passaporte:** "..Passport.."\n**Motivo:** "..Message.."\n**Address:** "..GetPlayerEndpoint(source),3092790)
-
-		if not vRP.HasGroup(Passport,"Admin") then
-			local Identity = vRP.Identity(Passport)
-			if Identity then
-				vRP.Kick(Passport,"Banido.")
-				vRP.Query("banneds/InsertBanned",{ license = Identity["license"], time = 999999 })
-			end
-		end
 	end
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -441,15 +424,6 @@ AddEventHandler("explosionEvent",function(source,Data)
 					if Expam[Token]["Count"] >= 10 then
 						local Passport = vRP.Passport(source)
 						TriggerEvent("Discord","Hackers","**Source:** "..source.."\n**Passaporte:** "..Passport.."\n**Motivo:** Spammou explosões.\n**Address:** "..GetPlayerEndpoint(source),3092790)
-
-						if not vRP.HasGroup(Passport,"Admin") then
-							local Identity = vRP.Identity(Passport)
-							if Identity then
-								vRP.Kick(Passport,"Banido.")
-								vRP.Query("banneds/InsertBanned",{ license = Identity["license"], time = 999999 })
-							end
-						end
-
 						CancelEvent()
 					end
 				end
@@ -483,15 +457,6 @@ AddEventHandler("weaponDamageEvent",function(source,Data)
 				if Tazer[Token]["Count"] >= 10 then
 					local Passport = vRP.Passport(source)
 					TriggerEvent("Discord","Hackers","**Source:** "..source.."\n**Passaporte:** "..Passport.."\n**Motivo:** Spammou tazer.\n**Address:** "..GetPlayerEndpoint(source),3092790)
-
-					if not vRP.HasGroup(Passport,"Admin") then
-						local Identity = vRP.Identity(Passport)
-						if Identity then
-							vRP.Kick(Passport,"Banido.")
-							vRP.Query("banneds/InsertBanned",{ license = Identity["license"], time = 999999 })
-						end
-					end
-
 					CancelEvent()
 				end
 			end
@@ -507,13 +472,5 @@ AddEventHandler("AnyResourceStop",function(Resource)
 	local Passport = vRP.Passport(source)
 	if Passport then
 		TriggerEvent("Discord","Hackers","**Source:** "..source.."\n**Passaporte:** "..Passport.."\n**Motivo:** Pausou o resource "..Resource.."\n**Address:** "..GetPlayerEndpoint(source),3092790)
-
-		if not vRP.HasGroup(Passport,"Admin") then
-			local Identity = vRP.Identity(Passport)
-			if Identity then
-				vRP.Kick(Passport,"Banido.")
-				vRP.Query("banneds/InsertBanned",{ license = Identity["license"], time = 999999 })
-			end
-		end
 	end
 end)

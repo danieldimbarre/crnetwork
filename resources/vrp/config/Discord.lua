a14930a1debaf5591bfe02d21baa3d19b8fc6202
Alan <alansoaressobral@gmail.com>
2022-10-26 20:21:50 -0300

    Discord

--- resources/vrp/config/Discord.lua
+++ resources/vrp/config/Discord.lua
@@ -9,7 +9,7 @@ Discords = {
 	["Police"] = "https://discord.com/api/webhooks/1022874777792282724/ImMiQ61Mc0u5dlHcLNRaaYx3TBKs0ODAvYo1CpEwM9Ht0aw3-7lU0dkQdrnR3BfcI5pw",
 	["Paramedic"] = "https://discord.com/api/webhooks/1022874820674867344/NlfNCvOqnwlPiD1Bf_wZrAOHFV9FeiwCJ28mqByFYMdDMEa1Y7qpcdzv9SuPMW9mjEaY",
 	["Gemstone"] = "https://discord.com/api/webhooks/1022874907358527599/EwrEt2FQDKc9i3EvHQFEx_oWgPTl4AXdZM_gNY5cCuWZ68-INHXeEAIbV-MimC8WHw8a",
-	["Login"] = "https://discord.com/api/webhooks/1024682305870114896/F2qoQtwiE358xLbF3rw1nUCfNtj-q5hvWcBk336cSEmM4OBZsz6RAZrxh3SmREssjwwa"
+	["Login"] = "https://discordapp.com/api/webhooks/1034966174531858543/uukZZBqyai-tX7av4Gisd_BvutPsRt-ZiDrpgtHKha0mlfhp6XcrHDDgYWFhiHMZMcvw"
 }
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- DISCORD

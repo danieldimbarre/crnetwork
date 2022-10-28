f31a3310865724c12eff8dcfe8cd12ca96a2abd1
Alan <alansoaressobral@gmail.com>
2022-10-28 17:09:56 -0300

    Survival

--- resources/[scripts]/survival/web-side/jquery.js
+++ resources/[scripts]/survival/web-side/jquery.js
@@ -2,11 +2,11 @@
 window.addEventListener("message",function(event){
 	switch (event["data"]["Action"]){
 		case "Display":
-			$("#Survival").css("display",event["data"]["Mode"]);
+			$("#SurvivalText").css("display",event["data"]["Mode"]);
 		break;
 
 		case "Message":
-			$("#Survival").html(event["data"]["Message"]);
+			$("#SurvivalText").html(event["data"]["Message"]);
 		break;
 	}
 });

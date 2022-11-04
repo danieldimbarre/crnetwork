d41bf4b3f7cd23dc79bfc68d4141076847d8bb8a
Alan <alansoaressobral@gmail.com>
2022-11-04 20:28:11 -0300

    Clothes

---
+++ resources/[addons]/energy-clothes/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game { 'gta5' }
+
+files {
+  'mp_m_freemode_01_mp_m_mc_roupas1.meta',
+  'mp_f_freemode_01_mp_f_mc_roupas1.meta',
+  'mp_m_freemode_01_mp_m_mc_policia.meta',
+  'mp_f_freemode_01_mp_f_mc_policia.meta'
+}
+
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_mc_roupas1.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_mc_roupas1.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_m_freemode_01_mp_m_mc_policia.meta'
+data_file 'SHOP_PED_APPAREL_META_FILE' 'mp_f_freemode_01_mp_f_mc_policia.meta'

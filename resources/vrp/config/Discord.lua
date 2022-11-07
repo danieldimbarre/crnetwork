9ddecb72a1f0710a974ba224ccf8e83bb7374953
Alan <alansoaressobral@gmail.com>
2022-11-07 19:33:36 -0300

    Discord

--- resources/vrp/config/Discord.lua
+++ resources/vrp/config/Discord.lua
@@ -2,10 +2,10 @@
 -- DISCORDS
 -----------------------------------------------------------------------------------------------------------------------------------------
 Discords = {
-	["Connect"] = "https://discord.com/api/webhooks/1022874276065464380/NkKAoIcWvipHev6haEqzvAYRHWzf8GMtyEh0NT8jzZSk5g3Ku0G_2OEERS5vAQeoFfV4",
-	["Disconnect"] = "https://discord.com/api/webhooks/1022874371833995264/t-0GcQHrBTuziykBbds2Zu1iFgjKG3ZZrGMxM6H2-ttnCdV04bn8Xm5oKF5hAZznsWF6",
-	["Airport"] = "https://discord.com/api/webhooks/1022874655486394378/JOuDg3YeKXB-DFwoT5IiUozBO2K2cWPX6G0QTJV-dhs--vy7_troN93pRd5Z2GhD_9mI",
-	["Deaths"] = "https://discord.com/api/webhooks/1022874690647244892/0oy4ge9hLAbWzYTViy3oa3vDONnktPpJoTW6mkB5E8zo8GXnvgDLPAbEYHYzGjnl6mZt",
+	["Connect"] = "https://discord.com/api/webhooks/1035718366821896232/JttUYqEC6gHTFG0eW-6hR4oMwqSa4Mwi3KBvqWNlVsuqxpwoIbwWgibaURKiG70Her6z",
+	["Disconnect"] = "https://discord.com/api/webhooks/1035719123159765062/0v2-JJ5xztffhpr50eSp-uzy_vfFdAQ63Rdg-heG6IAcJn87s_0sMEscSjhTo_wXLcXa",
+	["Airport"] = "https://discord.com/api/webhooks/1035719242970038374/4kVRLdCzfpOVT54Cllr9fmYt-zOdYw7zkUW6kB4_G7xiYptc2Mh7ls1iGo_9XHGwA1Gp",
+	["Deaths"] = "https://discord.com/api/webhooks/1035719352642703400/fC6uKw06EvXmkNUW7PcwkVK5q-EfN7_T3JzhFCyvoHp-9Bwx1ulkkHDyltdj-Wf0sLEJ",
 	["Police"] = "https://discord.com/api/webhooks/1022874777792282724/ImMiQ61Mc0u5dlHcLNRaaYx3TBKs0ODAvYo1CpEwM9Ht0aw3-7lU0dkQdrnR3BfcI5pw",
 	["Paramedic"] = "https://discord.com/api/webhooks/1022874820674867344/NlfNCvOqnwlPiD1Bf_wZrAOHFV9FeiwCJ28mqByFYMdDMEa1Y7qpcdzv9SuPMW9mjEaY",
 	["Gemstone"] = "https://discord.com/api/webhooks/1022874907358527599/EwrEt2FQDKc9i3EvHQFEx_oWgPTl4AXdZM_gNY5cCuWZ68-INHXeEAIbV-MimC8WHw8a",

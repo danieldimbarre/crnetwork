25fc9b5f98583b2bf2a0ee981f2c8a35903d3aca
Alan <alansoaressobral@gmail.com>
2022-11-07 21:40:11 -0300

    Update vRP + Bank

--- resources/[scripts]/bank/fxmanifest.lua
+++ resources/[scripts]/bank/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "bodacious"
 game "gta5"
 lua54 "yes"
-version "1.3.0"
+version "1.3.5"
 
 ui_page "web-side/index.html"
 
--- resources/vrp/fxmanifest.lua
+++ resources/vrp/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "bodacious"
 game "gta5"
 lua54 "yes"
-version "2.6.0"
+version "2.6.5"
 author "ImagicTheCat"
 creative_network "yes"
 

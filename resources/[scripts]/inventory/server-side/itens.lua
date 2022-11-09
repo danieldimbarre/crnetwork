e7f5773f9966a362fa88b13b4df4f6329acb6f65
Alan <alansoaressobral@gmail.com>
2022-11-09 15:16:21 -0300

    Premium

--- resources/[scripts]/inventory/server-side/itens.lua
+++ resources/[scripts]/inventory/server-side/itens.lua
@@ -4720,14 +4720,18 @@ Use = {
 	end,
 
 	["premium"] = function(source,Passport,Amount,Slot,Full,Item,Split)
-		if vRP.TakeItem(Passport,Full,1,true,Slot) then
-			TriggerClientEvent("inventory:Update",source,"Backpack")
-			TriggerEvent("Salary:Add",Passport,"Premium")
-			vRP.SetPermission(Passport,"Premium",1)
-
-			if not vRP.UserPremium(Passport) then
+		if not vRP.UserPremium(Passport) then
+			if vRP.TakeItem(Passport,Full,1,true,Slot) then
+				TriggerClientEvent("inventory:Update",source,"Backpack")
+				TriggerEvent("Salary:Add",Passport,"Premium")
+				vRP.SetPermission(Passport,"Premium",1)
 				vRP.SetPremium(source)
-			else
+			end
+		else
+			if vRP.TakeItem(Passport,Full,1,true,Slot) then
+				TriggerClientEvent("inventory:Update",source,"Backpack")
+				TriggerEvent("Salary:Add",Passport,"Premium")
+				vRP.SetPermission(Passport,"Premium",1)
 				vRP.UpgradePremium(source)
 			end
 		end

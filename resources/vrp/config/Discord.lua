33fa579428410b26ffc0114e28d8d497fd4d732b
Alan <alansoaressobral@gmail.com>
2022-11-10 16:28:53 -0300

    Discord

--- resources/vrp/config/Discord.lua
+++ resources/vrp/config/Discord.lua
@@ -9,7 +9,22 @@ Discords = {
 	["Police"] = "https://discord.com/api/webhooks/1022874777792282724/ImMiQ61Mc0u5dlHcLNRaaYx3TBKs0ODAvYo1CpEwM9Ht0aw3-7lU0dkQdrnR3BfcI5pw",
 	["Paramedic"] = "https://discord.com/api/webhooks/1022874820674867344/NlfNCvOqnwlPiD1Bf_wZrAOHFV9FeiwCJ28mqByFYMdDMEa1Y7qpcdzv9SuPMW9mjEaY",
 	["Gemstone"] = "https://discord.com/api/webhooks/1022874907358527599/EwrEt2FQDKc9i3EvHQFEx_oWgPTl4AXdZM_gNY5cCuWZ68-INHXeEAIbV-MimC8WHw8a",
-	["Login"] = "https://discordapp.com/api/webhooks/1034966174531858543/uukZZBqyai-tX7av4Gisd_BvutPsRt-ZiDrpgtHKha0mlfhp6XcrHDDgYWFhiHMZMcvw"
+	["Login"] = "https://discordapp.com/api/webhooks/1034966174531858543/uukZZBqyai-tX7av4Gisd_BvutPsRt-ZiDrpgtHKha0mlfhp6XcrHDDgYWFhiHMZMcvw",
+	["Mechanic"] = "https://discord.com/api/webhooks/1040345295734702150/BfBxAJzbyCDEGL6R8QmvZV7nVhC7-456QHVRzxgCUYR3pLCmuRTlA8d2pg446ROFUuN9",
+	["BurgerShot"] = "https://discord.com/api/webhooks/1040345536982679622/n4u5D8SV2oJMuFEUN2Ds4mYj7G8IU4DqwS_IAmosUaUOqSbw0C1Eef4KvMo41xDl9Ihv",
+	["PizzaThis"] = "https://discord.com/api/webhooks/1040345686148911144/IIIo8v065UXqfpbGkuElWc7ubdE92JndaFhzLyS5iIY3p2TAPazpM0P8mK-tMyzJIPwg",
+	["UwuCoffee"] = "https://discord.com/api/webhooks/1040345804264714281/KzsWhGqzgWK5UrRbdzO1Sl9BKX4y290kOXyi3eFHvFN2_uKBpkVc5uEKWHbCuDVBx1JK",
+	["BeanMachine"] = "https://discord.com/api/webhooks/1040346803884802128/FvwZ6JdiAs3twzjD3a0A-M9psB3kkW9558UcNC5bgO5cCZ1KYw58gezlGsazp0hx2u5J",
+	["Ballas"] = "https://discord.com/api/webhooks/1040346853276925992/zK9sgnvSELAj3xjni6zFf3BSXVXmVRnpOiYUv_-elitdx6eFA3c3Uhw49kWPcWJCQdJu",
+	["Vagos"] = "https://discord.com/api/webhooks/1040346905437278219/RItdYs7NKlL6YQJhxVzYaPUWqKsuKRvHk6ygIQXaeySUMV4f1EjFcBDD3HtFjuOfFImU",
+	["Families"] = "https://discord.com/api/webhooks/1040346945857798216/6hfj3VgG5TSqQCZK4kwlRIwsrvGv9vswt7_io2d0FnODrVHrU0QlO6069q4h5OldhvkB",
+	["Aztecas"] = "https://discord.com/api/webhooks/1040347020050841620/ad5FYHV1VQlZeuy2LgcnBjts3HuNPS-48OcuGfHuGj1zx2kLAmnGkcuzpEkshPBqavKS",
+	["Bloods"] = "https://discord.com/api/webhooks/1040347062228754491/p4T6PhUPddcMoEwD6ojDMz0w1RiMu55eNKH79lwNmbGcWrk0Kr-cSrMJVSgxZMfgQAfn",
+	["Triads"] = "https://discord.com/api/webhooks/1040347133099917432/CLLo28NInZVxhd0-3MoR3OeJQMU7DPlUSQNbyIqDYYNul7zPCeqY3hvogAgiCylpDGn9",
+	["Razors"] = "https://discord.com/api/webhooks/1040347170878009344/95LXNVOebUsdK4koEllNPMsZmb8HJxB4ge1r8sqfPOfO4ajcPWHHBCEgx-hZiLDXECiu",
+	["Altruists"] = "https://discord.com/api/webhooks/1040347207129374811/cMU_ytcCfS9_1uqzzxcM6KCiWNKKQ8rMMWz4ObFCL2tYQzagb75Ip7h-rvgOnBdgHZ43",
+	["Lost"] = "https://discord.com/api/webhooks/1040347237139619870/hXdrOs8a-6IiREA88MfBQyHkwJm27g-rip7MwFaIesIBmQn-Z6s62xxmWQqi3cgrd8Ac",
+	["Marabunta"] = "https://discord.com/api/webhooks/1040347269049884672/D4L2w8hThTDeMXJ2JqB-NwzY4mNk0OD5pFaM6PJC1VlqIRc8yUV_fRw-vlj6wJFz_KMR"
 }
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- DISCORD

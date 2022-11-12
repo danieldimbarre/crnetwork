6af3b0792498df93ff3869d79bf053983cace37f
Alan <alansoaressobral@gmail.com>
2022-11-12 13:45:50 -0300

    Desbugar

--- resources/[scripts]/barbershop/server-side/core.lua
+++ resources/[scripts]/barbershop/server-side/core.lua
@@ -52,16 +52,15 @@ AddEventHandler("barbershop:Debug",function()
 		local Datatable = vRP.Datatable(Passport)
 		if Datatable then
 			vRPC.Skin(source,Datatable["Skin"])
-			vRP.SkinCharacter(Passport,Datatable["Skin"])
+
+			TriggerClientEvent("barbershop:Apply",source,vRP.UserData(Passport,"Barbershop"))
+			TriggerClientEvent("skinshop:Apply",source,vRP.UserData(Passport,"Clothings"))
+			TriggerClientEvent("tattoos:Apply",source,vRP.UserData(Passport,"Tatuagens"))
+			TriggerClientEvent("target:Debug",source)
 
 			vRP.Revive(source,Datatable["Health"])
 			vRP.SetArmour(source,Datatable["Armour"])
 		end
-
-		TriggerClientEvent("barbershop:Apply",source,vRP.UserData(Passport,"Barbershop"))
-		TriggerClientEvent("skinshop:Apply",source,vRP.UserData(Passport,"Clothings"))
-		TriggerClientEvent("tattoos:Apply",source,vRP.UserData(Passport,"Tatuagens"))
-		TriggerClientEvent("target:Debug",source)
 
 		TriggerClientEvent("inventory:Cancel",source)
 

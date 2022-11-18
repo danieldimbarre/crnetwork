4b75648d7a75cc44d9fb266c2a5ab63b7f06aaaa
Alan <alansoaressobral@gmail.com>
2022-11-18 01:32:40 -0300

    Clothes

--- resources/[clothes]/energy-clothes-addons/fxmanifest.lua
+++ resources/[clothes]/energy-clothes-addons/fxmanifest.lua
@@ -12,5 +12,5 @@ data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_energy_police
 data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_mp_m_energy.meta"
 data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_energy.meta"
 
-data_file "SHOP_PED_APPAREL_META_FILE" "mp_m_freemode_01_mp_m_energy_jota.meta"
-data_file "SHOP_PED_APPAREL_META_FILE" "mp_f_freemode_01_mp_f_energy_jota.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_m_freemode_01_mp_m_energy_jota.meta"
+data_file "SHOP_PED_APPAREL_META_FILE" "data/mp_f_freemode_01_mp_f_energy_jota.meta"

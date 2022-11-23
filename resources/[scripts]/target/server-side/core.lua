dda775bbb1a7617cc9f24d4373db5157834519db
Alan <alansoaressobral@gmail.com>
2022-11-23 17:05:50 -0300

    Treatment

--- resources/[scripts]/target/server-side/core.lua
+++ resources/[scripts]/target/server-side/core.lua
@@ -17,14 +17,16 @@ function Creative.CheckIn()
 	local Passport = vRP.Passport(source)
 	if Passport then
 		if vRP.GetHealth(source) <= 100 then
-			if vRP.PaymentFull(Passport,975) then
-				vRP.UpgradeHunger(Passport,20)
-				vRP.UpgradeThirst(Passport,20)
-				TriggerEvent("Reposed",source,Passport,900)
+			if vRP.Request(source,"Prosseguir o tratamento por <b>$950</b> dólares?","Sim, iniciar tratamento","Não, volto mais tarde") then
+				if vRP.PaymentFull(Passport,975) then
+					vRP.UpgradeHunger(Passport,20)
+					vRP.UpgradeThirst(Passport,20)
+					TriggerEvent("Reposed",source,Passport,900)
 
-				return true
-			else
-				TriggerClientEvent("Notify",source,"vermelho","<b>Dólares</b> insuficientes.",5000)
+					return true
+				else
+					TriggerClientEvent("Notify",source,"vermelho","<b>Dólares</b> insuficientes.",5000)
+				end
 			end
 		else
 			if vRP.Request(source,"Prosseguir o tratamento por <b>$750</b> dólares?","Sim, iniciar tratamento","Não, volto mais tarde") then

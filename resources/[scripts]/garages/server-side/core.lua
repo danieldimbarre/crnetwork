b44f452b2cf56dd4dc44c2b314de7b45b937830e
Alan <alansoaressobral@gmail.com>
2022-11-23 21:24:04 -0300

    Garages

--- resources/[scripts]/garages/server-side/core.lua
+++ resources/[scripts]/garages/server-side/core.lua
@@ -519,6 +519,8 @@ AddEventHandler("garages:Spawn",function(Table)
 							if GetVehicleNumberPlateText(Network) == Plate then
 								vCLIENT.SearchBlip(source,GetEntityCoords(Network))
 								TriggerClientEvent("Notify",source,"amarelo","Rastreador do veículo foi ativado por <b>30</b> segundos, lembrando que se o mesmo estiver em movimento a localização pode ser imprecisa.",10000)
+							else
+								TriggerClientEvent("Notify",source,"amarelo","Rastreador está desativado.",5000)
 							end
 						else
 							if Spawn[Plate] then

9ee7db757de4d398024aaf8f3ed9b02934068867
Alan <alansoaressobral@gmail.com>
2022-11-24 01:19:46 -0300

    Chest

--- resources/[scripts]/chest/server-side/core.lua
+++ resources/[scripts]/chest/server-side/core.lua
@@ -323,7 +323,7 @@ function Creative.Update(Slot,Target,Amount)
 	if Passport and Open[Passport] then
 		if Amount <= 0 then Amount = 1 end
 
-		if vRP.Update(Passport,"Chest:"..Open[Passport]["Name"],Slot,Target,Amount) then
+		if vRP.UpdateChest(Passport,"Chest:"..Open[Passport]["Name"],Slot,Target,Amount) then
 			TriggerClientEvent("chest:Update",source,"Refresh")
 		end
 	end

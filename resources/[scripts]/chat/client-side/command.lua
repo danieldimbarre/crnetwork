6731bf879a83944643e373a69da626fee072e3bc
Alan <alansoaressobral@gmail.com>
2022-11-25 15:29:17 -0300

    Chat

--- resources/[scripts]/chat/client-side/command.lua
+++ resources/[scripts]/chat/client-side/command.lua
@@ -5,19 +5,12 @@ RegisterCommand("Chat",function()
 	if not IsPauseMenuActive() then
 		SetNuiFocus(true,true)
 
-		-- local Tags = {}
-
-		-- if LocalPlayer["state"]["Police"] then
-		-- 	Tags[#Tags + 1] = "Polícia"
-		-- end
-
-		-- if LocalPlayer["state"]["Mechanic"] then
-		-- 	Tags[#Tags + 1] = "Mecânica"
-		-- end
-
-		-- if LocalPlayer["state"]["Paramedic"] then
-		-- 	Tags[#Tags + 1] = "Hospital"
-		-- end
+		local Tags = {}
+		for Index,v in pairs(ClientState) do
+			if LocalPlayer["state"][Index] then
+				Tags[#Tags + 1] = Index
+			end
+		end
 
 		SendNUIMessage({ Action = "Chat", Data = Tags, Block = Block })
 	end
--- resources/[scripts]/chat/fxmanifest.lua
+++ resources/[scripts]/chat/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "bodacious"
 game "gta5"
 lua54 "yes"
-version "1.0.6"
+version "1.1.1"
 
 ui_page "web-side/index.html"
 
@@ -21,6 +21,7 @@ files {
 }
 
 shared_scripts {
+	"@vrp/config/Global.lua",
 	"shared-side/*"
 }
 

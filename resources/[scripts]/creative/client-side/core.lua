b55ff719d59cfedde25b7b45a571cfbf6ecca47d
Alan <alansoaressobral@gmail.com>
2022-12-01 13:02:13 -0300

    Codes

--- resources/[scripts]/creative/client-side/core.lua
+++ resources/[scripts]/creative/client-side/core.lua
@@ -231,9 +231,6 @@ local AudioFlag = {
 }
 
 CreateThread(function()
-	AddTextEntry("FE_THDR_GTAO","Energy")
-	ReplaceHudColour(116,18)
-
 	for _,v in pairs(Emitters) do
         SetStaticEmitterEnabled(v,false)
     end

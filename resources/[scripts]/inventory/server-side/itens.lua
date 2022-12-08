0d56318e3d0b5eed36f5d69f8c79f953a060820a
Alan <alansoaressobral@gmail.com>
2022-12-08 15:24:08 -0300

    Plants

--- resources/[scripts]/inventory/server-side/itens.lua
+++ resources/[scripts]/inventory/server-side/itens.lua
@@ -4067,7 +4067,7 @@ Use = {
 		Player(source)["state"]["Buttons"] = true
 		TriggerClientEvent("inventory:Close",source)
 
-		local Hash = "bag_coca_plant"
+		local Hash = "bkr_prop_weed_med_01b"
 		local Application,Coords = vRPC.objectCoords(source,Hash)
 		if Application then
 			if not vCLIENT.objectExist(source,Coords,Hash) then

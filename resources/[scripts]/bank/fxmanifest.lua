b16d6db63fdf482eb3e7fb4c258e20620292f721
Alan <alansoaressobral@gmail.com>
2022-12-13 14:29:59 -0300

    Update

--- resources/[scripts]/bank/fxmanifest.lua
+++ resources/[scripts]/bank/fxmanifest.lua
@@ -1,7 +1,7 @@
 fx_version "bodacious"
 game "gta5"
 lua54 "yes"
-version "1.3.5"
+version "1.5.0"
 
 ui_page "web-side/index.html"
 
--- resources/vrp/config/Global.lua
+++ resources/vrp/config/Global.lua
@@ -12,6 +12,7 @@ ServerName = "Energy"
 ReleaseText = "Envie na sala liberação"
 SpawnCoords = vec3(239.41,-1381.01,33.73)
 HookHacker = ""
+DiconnectServiceLeave = true
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- RESOURCES
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -67,4 +68,43 @@ ClientState = {
 	["Police"] = true,
 	["Mechanic"] = true,
 	["Paramedic"] = true
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- STARTCLOTHES
+-----------------------------------------------------------------------------------------------------------------------------------------
+StartClothes = {
+	["mp_m_freemode_01"] = {
+		["pants"] = { item = 0, texture = 0 },
+		["arms"] = { item = 0, texture = 0 },
+		["tshirt"] = { item = 1, texture = 0 },
+		["torso"] = { item = 0, texture = 0 },
+		["vest"] = { item = 0, texture = 0 },
+		["shoes"] = { item = 0, texture = 0 },
+		["mask"] = { item = 0, texture = 0 },
+		["backpack"] = { item = 0, texture = 0 },
+		["hat"] = { item = -1, texture = 0 },
+		["glass"] = { item = 0, texture = 0 },
+		["ear"] = { item = -1, texture = 0 },
+		["watch"] = { item = -1, texture = 0 },
+		["bracelet"] = { item = -1, texture = 0 },
+		["accessory"] = { item = 0, texture = 0 },
+		["decals"] = { item = 0, texture = 0 }
+	},
+	["mp_f_freemode_01"] = {
+		["pants"] = { item = 0, texture = 0 },
+		["arms"] = { item = 0, texture = 0 },
+		["tshirt"] = { item = 1, texture = 0 },
+		["torso"] = { item = 0, texture = 0 },
+		["vest"] = { item = 0, texture = 0 },
+		["shoes"] = { item = 0, texture = 0 },
+		["mask"] = { item = 0, texture = 0 },
+		["backpack"] = { item = 0, texture = 0 },
+		["hat"] = { item = -1, texture = 0 },
+		["glass"] = { item = 0, texture = 0 },
+		["ear"] = { item = -1, texture = 0 },
+		["watch"] = { item = -1, texture = 0 },
+		["bracelet"] = { item = -1, texture = 0 },
+		["accessory"] = { item = 0, texture = 0 },
+		["decals"] = { item = 0, texture = 0 }
+	}
 }

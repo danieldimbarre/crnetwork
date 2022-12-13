e2f6673ca510e94954e7605426c57ebda68cfeb6
Alan <alansoaressobral@gmail.com>
2022-12-13 16:41:41 -0300

    Bagdad

--- resources/[bagdad]/cfx-bagdad-props/fxmanifest.lua
+++ resources/[bagdad]/cfx-bagdad-props/fxmanifest.lua
@@ -2,8 +2,10 @@ fx_version "bodacious"
 game "gta5"
 
 files {
-    "stream/*",
-    "stream/*/**"
+	"stream/*",
+	"stream/*/**",
+	"stream/*/*/**",
+	"stream/*/*/*/**",
 }
 
 data_file "DLC_ITYP_REQUEST" "stream/cokeclone/bag_coca_plant.ytyp"

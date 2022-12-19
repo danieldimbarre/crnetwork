f180d8dac8070c793d9515a631a90fe76c90bb34
Alan <alansoaressobral@gmail.com>
2022-12-18 22:25:40 -0300

    Lscustoms

--- resources/[scripts]/lscustoms/server-side/core.lua
+++ resources/[scripts]/lscustoms/server-side/core.lua
@@ -40,7 +40,7 @@ AddEventHandler("lscustoms:attemptPurchase",function(type,mod)
 	local source = source
 	local Passport = vRP.Passport(source)
 	if Passport then
-		if type == "engines" or type == "brakes" or type == "transmission" or type == "suspension" then
+		if type == "engines" or type == "brakes" or type == "transmission" or type == "suspension" or type == "shield" then
 			local Price = vehicleCustomisationPrices[type][mod]
 
 			if vRP.PaymentFull(Passport,Price) then

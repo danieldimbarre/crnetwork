9dd906a3eca485799b89ea9353e9ca534d1ef47d
Alan <alansoaressobral@gmail.com>
2022-12-20 17:53:22 -0300

    Energetic

--- resources/[scripts]/inventory/server-side/itens.lua
+++ resources/[scripts]/inventory/server-side/itens.lua
@@ -665,7 +665,7 @@ Use = {
 
 					vRP.ChemicalTimer(Passport,10)
 					TriggerClientEvent("setCocaine",source)
-					TriggerClientEvent("setEnergetic",source,15,1.25 + (0.1 * Points))
+					TriggerClientEvent("setEnergetic",source,15,1.39 + (0.1 * Points))
 				end
 			end
 
@@ -2707,7 +2707,7 @@ Use = {
 				Active[Passport] = nil
 
 				if vRP.TakeItem(Passport,Full,1,true,Slot) then
-					TriggerClientEvent("setEnergetic",source,25,1.15)
+					TriggerClientEvent("setEnergetic",source,25,1.25)
 					vRP.UpgradeThirst(Passport,20)
 				end
 			end
@@ -3345,7 +3345,7 @@ Use = {
 
 				if vRP.TakeItem(Passport,Full,1,true,Slot) then
 					TriggerClientEvent("player:MushroomTea",source)
-					TriggerClientEvent("setEnergetic",source,25,1.15)
+					TriggerClientEvent("setEnergetic",source,25,1.25)
 					vRP.UpgradeThirst(Passport,20)
 				end
 			end

932bc3e8e4d1a11cd5a24e6bb40dcac7f079ef02
Alan <alansoaressobral@gmail.com>
2022-12-23 00:04:00 -0300

    Prison

--- resources/[scripts]/police/server-side/core.lua
+++ resources/[scripts]/police/server-side/core.lua
@@ -114,9 +114,10 @@ function cRP.initPrison(OtherPassport,Services,Value,Message)
 			if Identity then
 				local OtherSource = vRP.Source(OtherPassport)
 				if OtherSource then
-					vCLIENT.syncPrison(OtherSource,true,false)
+					vCLIENT.syncPrison(OtherSource,true,true)
 					TriggerClientEvent("radio:RadioClean",OtherSource)
-					TriggerClientEvent("Notify",OtherSource,"azul","Restam <b>"..Identity["prison"] + Services.." serviços</b>.",5000)
+					local OtherIdentity = vRP.Identity(OtherPassport)
+					TriggerClientEvent("Notify",OtherSource,"azul","Restam <b>"..OtherIdentity["prison"] + Services.." serviços</b>.",5000)
 				end
 
 				vRP.Query("prison/insertPrison",{ Police = Identity["name"].." "..Identity["name2"], Passport = parseInt(OtherPassport), Services = Services, Fines = Value, Text = Message, Date = os.date("%d/%m/%Y").." às "..os.date("%H:%M") })

3198d0d0b19cb22e1d1802ebf1cfe669d7a3df1d
Alan <alansoaressobral@gmail.com>
2022-12-26 17:35:36 -0300

    Robberys

--- resources/[scripts]/robberys/client-side/core.lua
+++ resources/[scripts]/robberys/client-side/core.lua
@@ -4,12 +4,12 @@
 RegisterNetEvent("robberys:Init")
 AddEventHandler("robberys:Init",function(Robberys)
 	for Number,v in pairs(Robberys) do
-		exports["target"]:AddCircleZone("Robberys:"..Number,v["Coords"],0.5,{
+		exports["target"]:AddCircleZone("Robberys:"..Number,v["Coords"],0.4,{
 			name = "Robberys:"..Number,
 			heading = 3374176
 		},{
 			shop = Number,
-			Distance = 1.0,
+			Distance = 0.7,
 			options = {
 				{
 					event = "robberys:Init",

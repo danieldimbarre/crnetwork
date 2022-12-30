a75d30a7fb5aa43f2ebebb2acfc628c0727d459a
Alan <alansoaressobral@gmail.com>
2022-12-30 13:25:46 -0300

    Vehicles

--- resources/[vehicles]/energy-vehicles/fxmanifest.lua
+++ resources/[vehicles]/energy-vehicles/fxmanifest.lua
@@ -6,68 +6,70 @@ client_script "client-side/*"
 files {
 	"audio/**/*",
 	"audio/**/**/*",
-	"data/*"
+	"data/**/*"
 }
 
-data_file "CARCOLS_FILE" "data/carcols.meta"
-data_file "POPSCHED_FILE" "data/popcycle.dat"
-data_file "HANDLING_FILE" "data/handling.meta"
-data_file "DLC_POP_GROUPS" "data/popgroups.ymt"
-data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
-data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
-data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
-
-data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
-data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
-data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
-data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/argento/dlc_argento"
-data_file "AUDIO_GAMEDATA" "audio/argento/argento_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/argento/argento_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/npolchar/dlc_npolchar"
-data_file "AUDIO_GAMEDATA" "audio/npolchar/npolchar_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/npolchar/npolchar_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/cvpiv8/dlc_cvpiv8"
-data_file "AUDIO_GAMEDATA" "audio/cvpiv8/cvpiv8_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/cvpiv8/cvpiv8_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/n4g63t/dlc_n4g63t"
-data_file "AUDIO_GAMEDATA" "audio/n4g63t/n4g63t_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/n4g63t/n4g63t_sounds.dat"
-
-data_file "AUDIO_WAVEPACK" "audio/f10m5/dlc_f10m5"
-data_file "AUDIO_GAMEDATA" "audio/f10m5/f10m5_game.dat"
-data_file "AUDIO_SOUNDDATA" "audio/f10m5/f10m5_sounds.dat"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"
 
 data_file "AUDIO_WAVEPACK" "audio/aqls7raceswap/dlc_aqls7raceswap"
 data_file "AUDIO_GAMEDATA" "audio/aqls7raceswap/aqls7raceswap_game.dat"
 data_file "AUDIO_SOUNDDATA" "audio/aqls7raceswap/aqls7raceswap_sounds.dat"
 
+data_file "AUDIO_WAVEPACK" "audio/argento/dlc_argento"
+data_file "AUDIO_GAMEDATA" "audio/argento/argento_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/argento/argento_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/cvpiv8/dlc_cvpiv8"
+data_file "AUDIO_GAMEDATA" "audio/cvpiv8/cvpiv8_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/cvpiv8/cvpiv8_sounds.dat"
+
 data_file "AUDIO_WAVEPACK" "audio/cw2019/dlc_cw2019"
 data_file "AUDIO_GAMEDATA" "audio/cw2019/cw2019_game.dat"
 data_file "AUDIO_SOUNDDATA" "audio/cw2019/cw2019_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/elegyx/dlc_elegyx"
+data_file "AUDIO_GAMEDATA" "audio/elegyx/elegyx_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/elegyx/elegyx_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/f10m5/dlc_f10m5"
+data_file "AUDIO_GAMEDATA" "audio/f10m5/f10m5_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/f10m5/f10m5_sounds.dat"
 
 data_file "AUDIO_WAVEPACK" "audio/fordvoodoo/dlc_fordvoodoo"
 data_file "AUDIO_GAMEDATA" "audio/fordvoodoo/fordvoodoo_game.dat"
 data_file "AUDIO_SOUNDDATA" "audio/fordvoodoo/fordvoodoo_sounds.dat"
 
+data_file "AUDIO_WAVEPACK" "audio/gresleyh/dlc_gresleyh"
+data_file "AUDIO_GAMEDATA" "audio/gresleyh/gresleyh_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/gresleyh/gresleyh_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/hemisound/dlc_hemisound"
+data_file "AUDIO_GAMEDATA" "audio/hemisound/hemisound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/hemisound/hemisound_sounds.dat"
+
 data_file "AUDIO_WAVEPACK" "audio/lambov10/dlc_lambov10"
 data_file "AUDIO_GAMEDATA" "audio/lambov10/lambov10_game.dat"
 data_file "AUDIO_SOUNDDATA" "audio/lambov10/lambov10_sounds.dat"
 
+data_file "AUDIO_WAVEPACK" "audio/n4g63t/dlc_n4g63t"
+data_file "AUDIO_GAMEDATA" "audio/n4g63t/n4g63t_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/n4g63t/n4g63t_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/npolchar/dlc_npolchar"
+data_file "AUDIO_GAMEDATA" "audio/npolchar/npolchar_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/npolchar/npolchar_sounds.dat"
+
 data_file "AUDIO_WAVEPACK" "audio/porsche57v10/dlc_porsche57v10"
 data_file "AUDIO_GAMEDATA" "audio/porsche57v10/porsche57v10_game.dat"
 data_file "AUDIO_SOUNDDATA" "audio/porsche57v10/porsche57v10_sounds.dat"
+
+data_file "AUDIO_WAVEPACK" "audio/r34sound/dlc_r34sound"
+data_file "AUDIO_GAMEDATA" "audio/r34sound/r34sound_game.dat"
+data_file "AUDIO_SOUNDDATA" "audio/r34sound/r34sound_sounds.dat"
 
 data_file "AUDIO_WAVEPACK" "audio/rotary7/dlc_rotary7"
 data_file "AUDIO_GAMEDATA" "audio/rotary7/rotary7_game.dat"

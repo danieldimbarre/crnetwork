6d12e9e5292e407790686b6598d0092766b8909d
Alan <alansoaressobral@gmail.com>
2023-01-07 10:15:16 -0300

    Doors

--- resources/[scripts]/doors/server-side/core.lua
+++ resources/[scripts]/doors/server-side/core.lua
@@ -96,8 +96,9 @@ GlobalState["Doors"] = {
 	-- [91] = { Coords = vec3(320.26,-574.34,43.43), Hash = -1700911976, Lock = true, Distance = 1.5, Perm = "Paramedic", Other = 90 },
 	-- [92] = { Coords = vec3(323.23,-575.42,43.43), Hash = -434783486, Lock = true, Distance = 1.5, Perm = "Paramedic", Other = 93 },
 	-- [93] = { Coords = vec3(325.65,-576.30,43.43), Hash = -1700911976, Lock = true, Distance = 1.5, Perm = "Paramedic", Other = 92 },
-	[94] = { Coords = vec3(475.39,-989.82,26.35), Hash = -692649124, Lock = true, Distance = 1.5, Perm = "Police-2" },
-
+	[94] = { Coords = vec3(475.39,-989.82,26.35), Hash = -692649124, Lock = true, Distance = 1.5, Perm = "Police" },
+	[95] = { Coords = vec3(384.43,-1601.95,30.04), Hash = -1335406364, Lock = true, Distance = 1.5, Perm = "Police" },
+	[96] = { Coords = vec3(374.64,-1613.63,30.04), Hash = -1335406364, Lock = true, Distance = 1.5, Perm = "Police" },
 	-- [101] = { Coords = vec3(805.03,-747.97,27.25), Hash = 95403626, Lock = true, Distance = 1.5, Perm = "PizzaThis", Other = 102 },
 	-- [102] = { Coords = vec3(803.98,-747.97,27.25), Hash = -49173194, Lock = true, Distance = 1.5, Perm = "PizzaThis", Other = 101 },
 	-- [103] = { Coords = vec3(794.29,-757.62,27.25), Hash = 95403626, Lock = true, Distance = 1.5, Perm = "PizzaThis", Other = 104 },

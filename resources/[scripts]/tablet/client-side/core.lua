27e9d94131b597c44f32132df5c54adebc58925c
Alan <alansoaressobral@gmail.com>
2023-01-13 12:33:23 -0300

    Exclusives

--- resources/[scripts]/tablet/client-side/core.lua
+++ resources/[scripts]/tablet/client-side/core.lua
@@ -191,7 +191,7 @@ local Vehicles = {
 	},{
 		["Coords"] = vec3(808.28,-905.06,25.68),
 		["heading"] = 308.98,
-		["Model"] = "gxrx7",
+		["Model"] = "er34h",
 		["Distance"] = 40,
 		["Tuning"] = true
 	}

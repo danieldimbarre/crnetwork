da1f84f6142ca68d08c1e3e11e3780feb9c58708
Alan <alansoaressobral@gmail.com>
2023-01-18 16:43:40 -0300

    Player

--- resources/[scripts]/player/server-side/core.lua
+++ resources/[scripts]/player/server-side/core.lua
@@ -188,7 +188,7 @@ RegisterCommand("p",function(source,Message,History)
 	local Passport = vRP.Passport(source)
 	if Passport and Message[1] and vRP.GetHealth(source) > 100 then
 		if vRP.HasService(Passport,"Police") then
-			TriggerClientEvent("Notify",-1,"verde",History:sub(2),,30000)
+			TriggerClientEvent("Notify",-1,"verde","O 1º BPM de Energy informa: "..History:sub(2),,30000)
 
 			TriggerEvent("Discord","Police","**Passaporte:** "..Passport.."\n**Mensagem:** "..History:sub(2),9807270)
 		end
@@ -201,7 +201,7 @@ RegisterCommand("h",function(source,Message,History)
 	local Passport = vRP.Passport(source)
 	if Passport and Message[1] and vRP.GetHealth(source) > 100 then
 		if vRP.HasService(Passport,"Paramedic") then
-			TriggerClientEvent("Notify",-1,"verde",History:sub(2),30000)
+			TriggerClientEvent("Notify",-1,"verde","O Centro Médico informa: "..History:sub(2),30000)
 
 			TriggerEvent("Discord","Paramedic","**Passaporte:** "..Passport.."\n**Mensagem:** "..History:sub(2),9807270)
 		end
@@ -214,7 +214,7 @@ RegisterCommand("m",function(source,Message,History)
 	local Passport = vRP.Passport(source)
 	if Passport and Message[1] and vRP.GetHealth(source) > 100 then
 		if vRP.HasGroup(Passport,"Mechanic") then
-			TriggerClientEvent("Notify",-1,"verde",History:sub(2),30000)
+			TriggerClientEvent("Notify",-1,"verde","A Mecânica informa: "..History:sub(2),30000)
 
 			TriggerEvent("Discord","Mechanic","**Passaporte:** "..Passport.."\n**Mensagem:** "..History:sub(2),9807270)
 		end

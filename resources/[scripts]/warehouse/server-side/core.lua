2896c66e3f85f569741590c26233480ebcc2dd8e
Alan <alansoaressobral@gmail.com>
2023-01-19 00:05:37 -0300

    Warehouse

--- resources/[scripts]/warehouse/server-side/core.lua
+++ resources/[scripts]/warehouse/server-side/core.lua
@@ -97,13 +97,17 @@ AddEventHandler("warehouse:Upgrade",function(Name)
 	local Passport = vRP.Passport(source)
 	if Passport then
 		local Warehouse = vRP.Query("warehouse/Informations",{ name = Name })
-		if Warehouse[1] then
+		if Warehouse[1] and Warehouse[1]["Passport"] == Passport then
 			if vRP.Request(source,"Aumentar <b>10kg</b> por <b>$10.000</b> dólares?","Sim, efetuar pagamento","Não, decido depois") then
-				if vRP.PaymentFull(Passport,10000) then
-					vRP.Query("warehouse/Upgrade",{ name = Name })
-					TriggerClientEvent("Notify",source,"verde","Aumento concluído.",3000)
+				if Warehouse[1]["weight"] + 10 <= 300 then
+					if vRP.PaymentFull(Passport,10000) then
+						vRP.Query("warehouse/Upgrade",{ name = Name })
+						TriggerClientEvent("Notify",source,"verde","Aumento concluído.",3000)
+					else
+						TriggerClientEvent("Notify",source,"vermelho","<b>Dólares</b> insuficientes.",5000)
+					end
 				else
-					TriggerClientEvent("Notify",source,"vermelho","<b>Dólares</b> insuficientes.",5000)
+					TriggerClientEvent("Notify",source,"vermelho","Limite atingido.",5000)
 				end
 			end
 		end
@@ -118,7 +122,7 @@ AddEventHandler("warehouse:Sell",function(Name)
 	local Passport = vRP.Passport(source)
 	if Passport then
 		local Warehouse = vRP.Query("warehouse/Informations",{ name = Name })
-		if Warehouse[1] then
+		if Warehouse[1] and Warehouse[1]["Passport"] == Passport then
 			if vRP.Request(source,"Deseja vender o armazém?","Sim, concluir a venda","Não, mudeia de ideia") then
 				vRP.RemSrvData("Warehouse:"..Name)
 

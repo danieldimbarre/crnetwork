0ce9608198bbf05404b523eb029a4bf44bf2e572
Alan <alansoaressobral@gmail.com>
2023-01-26 12:19:12 -0300

    Admin

--- resources/[scripts]/admin/server-side/core.lua
+++ resources/[scripts]/admin/server-side/core.lua
@@ -619,8 +619,9 @@ end)
 RegisterCommand("source",function(source,Message)
 	local Passport = vRP.Passport(source)
 	if Passport then
-		if vRP.HasGroup(Passport,"Admin") and parseInt(Message[1]) > 0 then
-			local OtherPassport = vRP.Passport(Message[1])
+		local Source = parseInt(Message[1])
+		if vRP.HasGroup(Passport,"Admin") and Source > 0 then
+			local OtherPassport = vRP.Passport(Source)
 			if OtherPassport then
 				TriggerClientEvent("Notify",source,"azul","<b>Passaporte:</b> "..OtherPassport,5000)
 				TriggerEvent("Discord","Admin","**source**\n\n**Passaporte:** "..Passport.."\n**Para:** "..OtherPassport,3553599)

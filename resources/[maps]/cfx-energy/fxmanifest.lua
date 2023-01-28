73b7c277548f30b40d9749cfa4da0368c3028b53
Alan <alansoaressobral@gmail.com>
2023-01-28 00:49:22 -0300

    Maps

--- resources/[maps]/cfx-energy/fxmanifest.lua
+++ resources/[maps]/cfx-energy/fxmanifest.lua
@@ -14,3 +14,14 @@ files {
 data_file "INTERIOR_PROXY_ORDER_FILE" "data/interiorproxies.meta"
 
 data_file "DLC_ITYP_REQUEST" "stream/Sign/vine_energy.ytyp"
+
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_02_brkk.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_bar_mlo.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_barbe_mlo.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_brkk_casas.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_brkk_props.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_fruta_mlo.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_meca_mlo.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_moto_mlo.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_roupas_mlo.ytyp"
+data_file "DLC_ITYP_REQUEST" "stream/Favelas/favela_sorve_mlo.ytyp"

bdd55077d8625e01a7d3aa16ef38ba81d7777e3b
Alan <alansoaressobral@gmail.com>
2023-01-30 12:32:22 -0300

    Notify

--- resources/[scripts]/notifyitens/client-side/core.lua
+++ resources/[scripts]/notifyitens/client-side/core.lua
@@ -5,6 +5,8 @@ RegisterNetEvent("itensNotify")
 AddEventHandler("itensNotify",function(status)
 	if status[1] == "recebeu" then
 		status[1] = "+"
+	elseif status[1] == "removeu" then
+		status[1] = "-"
 	end
 
 	SendNUIMessage({ mode = status[1], item = status[2], amount = parseFormat(status[3]), name = status[4] })

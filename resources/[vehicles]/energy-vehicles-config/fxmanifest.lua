d57ca5e5ac141b73d886fddd00f0d38096afab38
Alan <alansoaressobral@gmail.com>
2023-02-01 22:38:06 -0300

    Vehicles

---
+++ resources/[vehicles]/energy-vehicles-config/fxmanifest.lua
@@ -0,0 +1,18 @@
+fx_version "cerulean"
+game "gta5"
+lua54 "yes"
+
+client_script "client-side/*"
+
+files {
+  "data/*"
+}
+
+data_file "CARCOLS_FILE" "data/carcols.meta"
+data_file "HANDLING_FILE" "data/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+
+data_file "DLC_POP_GROUPS" "data/popgroups.ymt"
+data_file "POPSCHED_FILE" "data/popcycle.dat"

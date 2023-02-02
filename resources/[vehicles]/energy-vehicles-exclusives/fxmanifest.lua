9470b770ec1e32fcd7b6eb3d719dab95ac75f3f3
Alan <alansoaressobral@gmail.com>
2023-02-01 22:44:32 -0300

    Vehicles

--- resources/[vehicles]/energy-vehicles-exclusives/fxmanifest.lua
+++ resources/[vehicles]/energy-vehicles-exclusives/fxmanifest.lua
@@ -5,11 +5,11 @@ lua54 "yes"
 client_script "client-side/*"
 
 files {
-	"data/*"
+	"data/**/*"
 }
 
-data_file "CARCOLS_FILE" "data/carcols.meta"
-data_file "HANDLING_FILE" "data/handling.meta"
-data_file "VEHICLE_LAYOUTS_FILE" "data/vehiclelayouts.meta"
-data_file "VEHICLE_METADATA_FILE" "data/vehicles.meta"
-data_file "VEHICLE_VARIATION_FILE" "data/carvariations.meta"
+data_file "CARCOLS_FILE" "data/**/carcols.meta"
+data_file "HANDLING_FILE" "data/**/handling.meta"
+data_file "VEHICLE_LAYOUTS_FILE" "data/**/vehiclelayouts.meta"
+data_file "VEHICLE_METADATA_FILE" "data/**/vehicles.meta"
+data_file "VEHICLE_VARIATION_FILE" "data/**/carvariations.meta"

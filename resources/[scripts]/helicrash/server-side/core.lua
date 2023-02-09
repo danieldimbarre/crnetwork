edad5c1e6f5f991c4d214361e8d32d7754905f36
Alan <alansoaressobral@gmail.com>
2023-02-09 19:35:06 -0300

    Helicrash

--- resources/[scripts]/helicrash/server-side/core.lua
+++ resources/[scripts]/helicrash/server-side/core.lua
@@ -32,7 +32,9 @@ CreateThread(function()
 					for Slot,w in pairs(Loots[Loot]) do
 						local Durability = itemDurability(w["item"])
 						if Durability then
-							Loots[Loot][Slot]["item"] = Loots[Loot][Slot]["item"].."-"..parseInt(os.time() + (86400 * (Durability - (Durability / math.random(2,5)))))
+							w["item"] = w["item"].."-"..parseInt(os.time() - (86400 * (Durability / math.random(2,5))))
+
+							Loots[Loot][Slot] = w
 						end
 					end
 

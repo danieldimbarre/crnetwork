d4b1e54bfeccc4d0eeb32b223968dbf9979664f2
summeRz <contatosummerz@gmail.com>
2023-06-02 16:37:53 -0300

    Correção do Headshot.

--- Legacy/resources/[scripts]/metas/fxmanifest.lua
+++ Legacy/resources/[scripts]/metas/fxmanifest.lua
@@ -3,11 +3,8 @@ game "gta5"
 lua54 "yes"
 
 files {
-	"vehicle_weapons/*.meta",
-	"weaponcomponents/*.meta",
 	"weapons/*.meta"
 }
 
-data_file "WEAPONINFO_FILE_PATCH" "vehicle_weapons/*.meta"
-data_file "WEAPONCOMPONENTSINFO_FILE" "weaponcomponents/*.meta"
+data_file "WEAPONINFO_FILE" "weapons/*.meta"
 data_file "WEAPONINFO_FILE_PATCH" "weapons/*.meta"

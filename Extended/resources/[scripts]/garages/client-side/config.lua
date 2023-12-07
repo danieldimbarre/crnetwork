1b9b5265b71b3d18ea90d23ed678e307248f87a0
summeRz <contatosummerz@gmail.com>
2023-12-07 13:04:26 -0300

    Extended: Liberação da parte de modificações.

--- Extended/resources/[scripts]/garages/client-side/config.lua
+++ Extended/resources/[scripts]/garages/client-side/config.lua
@@ -231,3 +231,89 @@ Garages = {
 		["4"] = { 1272.11,-3266.03,6.10,91.00 }
 	}
 }
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- VEHICLEMODS
+-----------------------------------------------------------------------------------------------------------------------------------------
+function VehicleMods(Model,Vehicle,Customize)
+	if Customize then
+		SetVehicleModKit(Vehicle,0)
+
+		if Customize["wheeltype"] ~= nil then
+			SetVehicleWheelType(Vehicle,Customize["wheeltype"])
+		end
+
+		if Customize["mods"] then
+			for i = 0,16 do
+				if Customize["mods"][tostring(i)] ~= nil then
+					SetVehicleMod(Vehicle,i,Customize["mods"][tostring(i)])
+				end
+			end
+
+			for i = 17,22 do
+				if Customize["mods"][tostring(i)] ~= nil then
+					ToggleVehicleMod(Vehicle,i,Customize["mods"][tostring(i)])
+				end
+			end
+
+			for i = 23,24 do
+				if Customize["mods"][tostring(i)] ~= nil then
+					if not Customize["var"] then
+						Customize["var"] = {}
+						Customize["var"][tostring(i)] = 0
+					end
+
+					SetVehicleMod(Vehicle,i,Customize["mods"][tostring(i)],Customize["var"][tostring(i)])
+				end
+			end
+
+			for i = 25,48 do
+				if Customize["mods"][tostring(i)] ~= nil then
+					SetVehicleMod(Vehicle,i,Customize["mods"][tostring(i)])
+				end
+			end
+		end
+
+		if Customize["neon"] ~= nil then
+			for i = 0,3 do
+				SetVehicleNeonLightEnabled(Vehicle,i,Customize["neon"][tostring(i)])
+			end
+		end
+
+		if Customize["extras"] ~= nil then
+			for i = 1,12 do
+				local onoff = tonumber(Customize["extras"][i])
+				if onoff == 1 then
+					SetVehicleExtra(Vehicle,i,0)
+				else
+					SetVehicleExtra(Vehicle,i,1)
+				end
+			end
+		end
+
+		if Customize["liverys"] ~= nil and Customize["liverys"] ~= 24  then
+			SetVehicleLivery(Vehicle,Customize["liverys"])
+		end
+
+		if Customize["plateIndex"] ~= nil and Customize["plateIndex"] ~= 4 then
+			SetVehicleNumberPlateTextIndex(Vehicle,Customize["plateIndex"])
+		end
+
+		SetVehicleXenonLightsColour(Vehicle,Customize["xenonColor"])
+		SetVehicleColours(Vehicle,Customize["colors"][1],Customize["colors"][2])
+		SetVehicleExtraColours(Vehicle,Customize["extracolors"][1],Customize["extracolors"][2])
+		SetVehicleNeonLightsColour(Vehicle,Customize["lights"][1],Customize["lights"][2],Customize["lights"][3])
+		SetVehicleTyreSmokeColor(Vehicle,Customize["smokecolor"][1],Customize["smokecolor"][2],Customize["smokecolor"][3])
+
+		if Customize["tint"] ~= nil then
+			SetVehicleWindowTint(Vehicle,Customize["tint"])
+		end
+
+		if Customize["dashColour"] ~= nil then
+			SetVehicleInteriorColour(Vehicle,Customize["dashColour"])
+		end
+
+		if Customize["interColour"] ~= nil then
+			SetVehicleDashboardColour(Vehicle,Customize["interColour"])
+		end
+	end
+end

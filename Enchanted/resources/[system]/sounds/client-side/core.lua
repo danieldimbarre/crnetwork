5d5adabd70cd12318fa0f4e6b47292b609986e0c
summeRz <contatosummerz@gmail.com>
2024-06-29 20:18:11 -0300

    Enchanted: Update sounds.

--- Enchanted/resources/[system]/sounds/client-side/core.lua
+++ Enchanted/resources/[system]/sounds/client-side/core.lua
@@ -1,4 +1,18 @@
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- SOUNDS:PRIVATE
+-----------------------------------------------------------------------------------------------------------------------------------------
 RegisterNetEvent("sounds:Private")
-AddEventHandler("sounds:Private",function(sound,volume)
-	SendNUIMessage({ transactionType = "playSound", transactionFile = sound, transactionVolume = volume })
+AddEventHandler("sounds:Private",function(Sound,Volume)
+	SendNUIMessage({ transactionType = "playSound", transactionFile = Sound, transactionVolume = Volume })
+end)
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- SOUNDS:AREA
+-----------------------------------------------------------------------------------------------------------------------------------------
+RegisterNetEvent("sounds:Area")
+AddEventHandler("sounds:Area",function(Sound,Volume,OtherCoords,Distance,Route)
+	local Ped = PlayerPedId()
+	local Coords = GetEntityCoords(Ped)
+	if LocalPlayer["state"]["Route"] == Route and #(Coords - OtherCoords) <= Distance then
+		SendNUIMessage({ transactionType = "playSound", transactionFile = Sound, transactionVolume = Volume })
+	end
 end)

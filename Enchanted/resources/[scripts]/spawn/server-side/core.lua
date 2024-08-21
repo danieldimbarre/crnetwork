5c76cb027a4186167da24e9cec9f7b965ab03cdc
summeRz <contatosummerz@gmail.com>
2024-08-21 20:24:54 -0300

    Enchanted: Migrado o bloqueio para o spawn.

--- Enchanted/resources/[scripts]/spawn/server-side/core.lua
+++ Enchanted/resources/[scripts]/spawn/server-side/core.lua
@@ -15,6 +15,8 @@ Tunnel.bindInterface("spawn",Creative)
 -----------------------------------------------------------------------------------------------------------------------------------------
 local Active = {}
 local Route = 50000
+local Connected = {}
+local Licensed = {}
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CHARACTERS
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -49,24 +51,27 @@ end
 -- CHARACTERCHOSEN
 -----------------------------------------------------------------------------------------------------------------------------------------
 function Creative.CharacterChosen(Passport)
+	local Return = false
 	local source = source
 	local License = vRP.Identities(source)
 	local Consult = vRP.Query("characters/UserLicense",{ Passport = Passport, License = License })
 
-	if Consult[1] then
+	if Consult[1] and not Licensed[License] then
 		vRP.CharacterChosen(source,Passport)
-
-		return true
+		Connected[Passport] = License
+		Licensed[License] = true
+		Return = true
 	else
-		DropPlayer(source,"Desconectado.")
+		DropPlayer(source,"Desconectado")
 	end
 
-	return false
+	return Return
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- NEWCHARACTER
 -----------------------------------------------------------------------------------------------------------------------------------------
 function Creative.NewCharacter(Name,Lastname,Sex)
+	local Return = false
 	local source = source
 	if not Active[source] then
 		Active[source] = true
@@ -76,26 +81,39 @@ function Creative.NewCharacter(Name,Lastname,Sex)
 
 		if Account["Characters"] <= vRP.Scalar("characters/Count",{ License = License }) then
 			TriggerClientEvent("Notify",source,"Atenção","Limite de personagem atingido.","amarelo",5000)
-			Active[source] = nil
+		else
+			local Sexo = "M"
+			if Sex == "mp_f_freemode_01" then
+				Sexo = "F"
+			end
 
-			return false
-		end
+			Return = true
+			vRPC.DoScreenFadeOut(source)
+			vRP.Query("characters/NewCharacter",{ License = License, Name = Name, Lastname = Lastname, Sex = Sexo, Skin = Sex, Blood = math.random(4) })
 
-		local Sexo = "M"
-		if Sex == "mp_f_freemode_01" then
-			Sexo = "F"
-		end
-
-		vRPC.DoScreenFadeOut(source)
-		vRP.Query("characters/NewCharacter",{ License = License, Name = Name, Lastname = Lastname, Sex = Sexo, Skin = Sex, Blood = math.random(4) })
-
-		local Consult = vRP.Query("characters/LastCharacter",{ License = License })
-		if Consult[1] then
-			vRP.CharacterChosen(source,Consult[1]["id"],Sex)
+			local Consult = vRP.Query("characters/LastCharacter",{ License = License })
+			if Consult[1] then
+				vRP.CharacterChosen(source,Consult[1]["id"],Sex)
+			end
 		end
 
 		Active[source] = nil
-
-		return true
 	end
+
+	return Return
 end
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- DISCONNECT
+-----------------------------------------------------------------------------------------------------------------------------------------
+AddEventHandler("Disconnect",function(Passport,source)
+    if Connected[Passport] then
+    	local License = Connected[Passport]
+
+        Connected[Passport] = nil
+        Licensed[License] = nil
+    end
+
+    if Active[source] then
+    	Active[source] = nil
+    end
+end)

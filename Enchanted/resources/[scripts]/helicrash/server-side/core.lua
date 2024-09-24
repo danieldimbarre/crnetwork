673e07f956fb226b18a8d0c1c7cbcfdbcd39b277
summeRz <contatosummerz@gmail.com>
2024-09-23 22:57:19 -0300

    Enchanted: Atualização e correção no helicrash.

--- Enchanted/resources/[scripts]/helicrash/server-side/core.lua
+++ Enchanted/resources/[scripts]/helicrash/server-side/core.lua
@@ -14,18 +14,19 @@ GlobalState["Helicrash"] = false
 -----------------------------------------------------------------------------------------------------------------------------------------
 RegisterCommand("helicrash",function(source,Message)
 	local Passport = vRP.Passport(source)
-	if Passport and not GlobalState["Helicrash"] and vRP.HasGroup(Passport,"Admin") then
-		local Selected = math.random(#Components)
+	if Passport and not GlobalState["Helicrash"] and vRP.HasGroup(Passport,"Admin") and Message[1] then
+		local Selected = parseInt(Message[1])
+		if Components[Selected] then
+			for Number = 1,CountTable(Components[Selected]["Coords"]) do
+				TriggerEvent("chest:Cooldown","Helicrash:"..Number)
+				vRP.MountContainer(Passport,"Helicrash:"..Number,Loots,math.random(3,5))
+			end
 
-		for Number = 1,CountTable(Components[Selected]["Coords"]) do
-			TriggerEvent("chest:Cooldown","Helicrash:"..Number)
-			vRP.MountContainer(Passport,"Helicrash:"..Number,Loots,math.random(3,5))
+			TriggerClientEvent("Notify",-1,"Queda da Aeronave","Mayday! Mayday! Tivemos problemas técnicos em nossos motores e estamos em queda livre.","ilegal",30000)
+			GlobalState["Helibox"] = CountTable(Components[Selected]["Coords"])
+			GlobalState["Helifire"] = GlobalState["Work"] + 60
+			GlobalState["Helicrash"] = Selected
 		end
-
-		TriggerClientEvent("Notify",-1,"Queda da Aeronave","Mayday! Mayday! Tivemos problemas técnicos em nossos motores e estamos em queda livre.","ilegal",30000)
-		GlobalState["Helibox"] = CountTable(Components[Selected]["Coords"])
-		GlobalState["Helifire"] = GlobalState["Work"] + 60
-		GlobalState["Helicrash"] = Selected
 	end
 end)
 -----------------------------------------------------------------------------------------------------------------------------------------

a92e0982aee599fd283bc116aa4db42e225c923f
summeRz <contatosummerz@gmail.com>
2024-09-25 21:49:11 -0300

    Enchanted: Atualizando as texturas de acordo com os sprays.

--- Enchanted/resources/vrp/client/base.lua
+++ Enchanted/resources/vrp/client/base.lua
@@ -26,11 +26,11 @@ end)
 -----------------------------------------------------------------------------------------------------------------------------------------
 CreateThread(function()
 	local YTD = CreateRuntimeTxd("Textures")
-	local Textures = vRPS.FilesDirectory("resources/vrp/config/textures/target")
+	local Textures = vRPS.FilesDirectory("resources/vrp/config/textures")
 
 	for _,Name in pairs(Textures) do
 		local TEXTURE = CreateRuntimeTexture(YTD,Name,512,512)
-		local PNG = LoadResourceFile("vrp","config/textures/target/"..Name..".png")
+		local PNG = LoadResourceFile("vrp","config/textures/"..Name..".png")
 		local DICT = "data:image/png;base64,"..Base64(PNG)
 
 		SetRuntimeTextureImage(TEXTURE,DICT)

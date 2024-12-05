00fa947a20bcbdd8ff2047af90943298bee83e98
summeRz <contatosummerz@gmail.com>
2024-12-05 18:58:13 -0300

    Enchanted: Megazord fix.

--- Enchanted/resources/[scripts]/megazord/server-side/config.lua
+++ Enchanted/resources/[scripts]/megazord/server-side/config.lua
@@ -93,17 +93,49 @@ Explodes = {
 -- BANNEDMODELS
 -----------------------------------------------------------------------------------------------------------------------------------------
 BannedModels = {
-	GetHashKey("jet") = true,
-	GetHashKey("besra") = true,
-	GetHashKey("luxor") = true,
-	GetHashKey("blimp") = true,
-	GetHashKey("polmav") = true,
-	GetHashKey("buzzard2") = true,
-	GetHashKey("mammatus") = true,
-	GetHashKey("s_m_y_prismuscl_01") = true,
-	GetHashKey("u_m_y_prisoner_01") = true,
-	GetHashKey("s_m_y_prisoner_01") = true,
-	GetHashKey("frogger") = true
+	[`jet`] = true,
+	[`besra`] = true,
+	[`luxor`] = true,
+	[`blimp`] = true,
+	[`polmav`] = true,
+	[`buzzard2`] = true,
+	[`mammatus`] = true,
+	[`s_m_y_prismuscl_01`] = true,
+	[`u_m_y_prisoner_01`] = true,
+	[`s_m_y_prisoner_01`] = true,
+	[`frogger`] = true
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- PARTICLES
+-----------------------------------------------------------------------------------------------------------------------------------------
+Particles = {
+	[`scr_firework_xmas_burst_rgw`] = true,
+	[`exp_grd_petrol_pump`] = true,
+	[`scr_clown_bul`] = true,
+	[`scr_mich4_firework_trailburst_spawn`] = true,
+	[`blood_shark_attack`] = true,
+	[`td_blood_shotgun`] = true,
+	[`td_blood_throat`] = true,
+	[`blood_melee_punch`] = true,
+	[`blood_exit`] = true,
+	[`blood_chopper`] = true,
+	[`trail_splash_blood`] = true,
+	[`td_blood_stab`] = true,
+	[`blood_armour_heavy`] = true,
+	[`blood_mist`] = true,
+	[`blood_nose`] = true,
+	[`blood_entry_head_sniper`] = true,
+	[`scr_ba_bomb_destroy`] = true,
+	[`scr_clown_appears`] = true
+}
+-----------------------------------------------------------------------------------------------------------------------------------------
+-- ASSETS
+-----------------------------------------------------------------------------------------------------------------------------------------
+Assets = {
+	[`core`] = true,
+	[`scr_ba_bomb`] = true,
+	[`scr_rcbarry2`] = true,
+	[`scr_rcpaparazzo1`] = true
 }
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- HACKEREVENTS
@@ -473,36 +505,4 @@ HackerEvents = {
 	"InteractSound_SV:PlayOnAll",
 	"SEM_InteractionMenu:Jail",
 	"SEM_InteractionMenu:DragNear"
-}
------------------------------------------------------------------------------------------------------------------------------------------
--- PARTICLES
------------------------------------------------------------------------------------------------------------------------------------------
-Particles = {
-	GetHashKey("scr_firework_xmas_burst_rgw"),
-	GetHashKey("exp_grd_petrol_pump"),
-	GetHashKey("scr_clown_bul"),
-	GetHashKey("scr_mich4_firework_trailburst_spawn"),
-	GetHashKey("blood_shark_attack"),
-	GetHashKey("td_blood_shotgun"),
-	GetHashKey("td_blood_throat"),
-	GetHashKey("blood_melee_punch"),
-	GetHashKey("blood_exit"),
-	GetHashKey("blood_chopper"),
-	GetHashKey("trail_splash_blood"),
-	GetHashKey("td_blood_stab"),
-	GetHashKey("blood_armour_heavy"),
-	GetHashKey("blood_mist"),
-	GetHashKey("blood_nose"),
-	GetHashKey("blood_entry_head_sniper"),
-	GetHashKey("scr_ba_bomb_destroy"),
-	GetHashKey("scr_clown_appears")
-}
------------------------------------------------------------------------------------------------------------------------------------------
--- ASSETS
------------------------------------------------------------------------------------------------------------------------------------------
-Assets = {
-	GetHashKey("core"),
-	GetHashKey("scr_ba_bomb"),
-	GetHashKey("scr_rcbarry2"),
-	GetHashKey("scr_rcpaparazzo1")
 }

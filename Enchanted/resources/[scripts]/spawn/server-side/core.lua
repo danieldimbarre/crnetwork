9ed8ce83100a19e5b02079fe8c9013283cf6a538
summeRz <contatosummerz@gmail.com>
2024-12-06 14:39:29 -0300

    Enchanted: Atualização no sistema de spawn.

--- Enchanted/resources/[scripts]/spawn/server-side/core.lua
+++ Enchanted/resources/[scripts]/spawn/server-side/core.lua
@@ -14,8 +14,8 @@ Tunnel.bindInterface("spawn",Creative)
 -- VARIABLES
 -----------------------------------------------------------------------------------------------------------------------------------------
 local Active = {}
-local Connected = {}
 local Licensed = {}
+local Connected = {}
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- CHARACTERS
 -----------------------------------------------------------------------------------------------------------------------------------------
@@ -23,28 +23,30 @@ function Creative.Characters()
 	local Characters = {}
 	local source = source
 	local License = vRP.Identities(source)
-	if License then
-		local Consult = vRP.Query("characters/Characters",{ License = License })
 
-		exports["vrp"]:Bucket(source,"Enter",50000 + source)
-
-		for _,v in pairs(Consult) do
-			local Passport = parseInt(v["id"])
-
-			Characters[#Characters + 1] = {
-				["Passport"] = Passport,
-				["Skin"] = v["Skin"],
-				["Nome"] = v["Name"].." "..v["Lastname"],
-				["Sexo"] = v["Sex"],
-				["Banco"] = v["Bank"],
-				["Blood"] = Sanguine(v["Blood"]),
-				["Clothes"] = vRP.UserData(Passport,"Clothings"),
-				["Barber"] = vRP.UserData(Passport,"Barbershop"),
-				["Tattoos"] = vRP.UserData(Passport,"Tattooshop")
-			}
-		end
-	else
+	if not License then
 		DropPlayer(source,"Não foi possível efetuar conexão com a "..(BaseMode == "steam" and "Steam" or "Rockstar")..".")
+
+		return Characters
+	end
+
+	exports["vrp"]:Bucket(source,"Enter",50000 + source)
+
+	local Consult = vRP.Query("characters/Characters",{ License = License })
+	for _,v in ipairs(Consult) do
+		local Passport = tonumber(v.id)
+
+		table.insert(Characters,{
+			Passport = Passport,
+			Skin = v.Skin,
+			Nome = v.Name.." "..v.Lastname,
+			Sexo = v.Sex,
+			Banco = v.Bank,
+			Blood = Sanguine(v.Blood),
+			Clothes = vRP.UserData(Passport,"Clothings"),
+			Barber = vRP.UserData(Passport,"Barbershop"),
+			Tattoos = vRP.UserData(Passport,"Tattooshop")
+		})
 	end
 
 	return Characters
@@ -53,75 +55,86 @@ end
 -- CHARACTERCHOSEN
 -----------------------------------------------------------------------------------------------------------------------------------------
 function Creative.CharacterChosen(Passport)
-	local Return = false
 	local source = source
 	local License = vRP.Identities(source)
-	if License then
-		local Consult = vRP.Query("characters/UserLicense",{ Passport = Passport, License = License })
-		if Consult[1] and not Licensed[License] then
-			vRP.CharacterChosen(source,Passport)
-			Connected[Passport] = License
-			Licensed[License] = true
-			Return = true
-		else
-			DropPlayer(source,"Não foi possível efetuar conexão com a "..(BaseMode == "steam" and "Steam" or "Rockstar")..".")
-		end
-	else
+	if not License then
 		DropPlayer(source,"Não foi possível efetuar conexão com a "..(BaseMode == "steam" and "Steam" or "Rockstar")..".")
+
+		return false
 	end
 
-	return Return
+	local Consult = vRP.Query("characters/UserLicense",{ Passport = Passport, License = License })
+	if Consult[1] and not Licensed[License] then
+		vRP.CharacterChosen(source,Passport)
+		Connected[Passport] = License
+		Licensed[License] = true
+
+		return true
+	end
+
+	DropPlayer(source,"Não foi possível efetuar conexão com a "..(BaseMode == "steam" and "Steam" or "Rockstar")..".")
+
+	return false
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- NEWCHARACTER
 -----------------------------------------------------------------------------------------------------------------------------------------
 function Creative.NewCharacter(Name,Lastname,Sex)
-	local Return = false
 	local source = source
-	if not Active[source] then
-		Active[source] = true
-
-		local License = vRP.Identities(source)
-		if License then
-			local Account = vRP.Account(License)
-			if Account and Account["Characters"] <= vRP.Scalar("characters/Count",{ License = License }) then
-				TriggerClientEvent("Notify",source,"Atenção","Limite de personagem atingido.","amarelo",5000)
-			else
-				local Sexo = "M"
-				if Sex == "mp_f_freemode_01" then
-					Sexo = "F"
-				end
-
-				Return = true
-				vRPC.DoScreenFadeOut(source)
-				vRP.Query("characters/NewCharacter",{ License = License, Name = EmptySpace(Name), Lastname = EmptySpace(Lastname), Sex = Sexo, Skin = Sex, Blood = math.random(4) })
-
-				local Consult = vRP.Query("characters/LastCharacter",{ License = License })
-				if Consult[1] then
-					vRP.CharacterChosen(source,Consult[1]["id"],Sex)
-				end
-			end
-		else
-			DropPlayer(source,"Não foi possível efetuar conexão com a "..(BaseMode == "steam" and "Steam" or "Rockstar")..".")
-		end
-
-		Active[source] = nil
+	if Active[source] then
+		return false
 	end
 
-	return Return
+	Active[source] = true
+
+	local License = vRP.Identities(source)
+	if not License then
+		DropPlayer(source,"Não foi possível efetuar conexão com a "..(BaseMode == "steam" and "Steam" or "Rockstar")..".")
+		Active[source] = nil
+
+		return false
+	end
+
+	local Account = vRP.Account(License)
+	if Account and Account.Characters <= vRP.Scalar("characters/Count",{ License = License }) then
+		TriggerClientEvent("Notify",source,"Atenção","Limite de personagem atingido.","amarelo",5000)
+		Active[source] = nil
+
+		return false
+	end
+
+	local Sexo = (Sex == "mp_f_freemode_01") and "F" or "M"
+
+	vRP.Query("characters/NewCharacter",{
+		License = License,
+		Name = FirstText(Name),
+		Lastname = FirstText(Lastname),
+		Sex = Sexo,
+		Skin = Sexo,
+		Blood = math.random(4)
+	})
+
+	local Consult = vRP.Query("characters/LastCharacter",{ License = License })
+	if Consult[1] then
+		vRPC.DoScreenFadeOut(source)
+		vRP.CharacterChosen(source,Consult[1].id,Sexo)
+	end
+
+	Active[source] = nil
+
+	return true
 end
 -----------------------------------------------------------------------------------------------------------------------------------------
 -- DISCONNECT
 -----------------------------------------------------------------------------------------------------------------------------------------
 AddEventHandler("Disconnect",function(Passport,source)
-    if Connected[Passport] then
-    	local License = Connected[Passport]
+	local License = Connected[Passport]
+	if License then
+		Connected[Passport] = nil
+		Licensed[License] = nil
+	end
 
-        Connected[Passport] = nil
-        Licensed[License] = nil
-    end
-
-    if Active[source] then
-    	Active[source] = nil
-    end
+	if Active[source] then
+		Active[source] = nil
+	end
 end)

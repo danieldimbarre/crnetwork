25cf726488b43fbdfed8cac3b9392e69bdbceb40
summeRz <contatosummerz@gmail.com>
2025-03-25 16:11:12 -0300

    Enchanted: Prepare do Jester.

--- Enchanted/resources/vrp/modules/prepare.lua
+++ Enchanted/resources/vrp/modules/prepare.lua
@@ -4,6 +4,7 @@
 vRP.Prepare("characters/Person","SELECT * FROM characters WHERE id = @Passport")
 vRP.Prepare("characters/Delete","UPDATE characters SET Deleted = 1 WHERE id = @Passport")
 vRP.Prepare("characters/SetSkin","UPDATE characters SET Skin = @Skin WHERE id = @Passport")
+vRP.Prepare("characters/UpdatePhone","UPDATE characters SET Phone = @Phone WHERE id = @Passport")
 vRP.Prepare("characters/UpdateDaily","UPDATE characters SET Daily = @Daily WHERE id = @Passport")
 vRP.Prepare("characters/SetMedicplan","UPDATE characters SET Medic = @Medic WHERE id = @Passport")
 vRP.Prepare("characters/AddBank","UPDATE characters SET Bank = Bank + @Bank WHERE id = @Passport")
